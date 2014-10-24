99884570c79bf53528229d3976dd0e8011d71265
costd035 <daniel.x.costinett.-nd@disney.com>
2014-10-24 15:38:28 -0700

    fix stateName value on scroll

--- app/src/main/java/net/cozz/danco/homework2/OptimizedCustomAdapter.java
+++ app/src/main/java/net/cozz/danco/homework2/OptimizedCustomAdapter.java
@@ -2,73 +2,83 @@ package net.cozz.danco.homework2;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import java.util.List;
 import java.util.Random;
 
 /**
  * Created by costd035 on 10/22/14.
  */
 public class OptimizedCustomAdapter extends ArrayAdapter<String> {
+    private static final String TAG = "OptimizedCustomAdapter";
 
-    private static ViewHolder viewHolder;
     private Context context;
     private String[] values;
 
 
     public OptimizedCustomAdapter(Context context, int resource, String[] values) {
         super(context, resource, values);
-        viewHolder = new ViewHolder();
         this.context = context;
         this.values = values;
     }
 
     public OptimizedCustomAdapter(Context context, String[] values) {
         super(context, R.layout.row_layout, values);
-        viewHolder = new ViewHolder();
         this.context = context;
         this.values = values;
     }
 
     static class ViewHolder {
-        static View view;
+        public TextView stateName;
+        public View leftBox;
+        public View rightBox;
     }
 
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        ViewHolder viewHolder;
+
         if (convertView == null) {
             // inflate the view and put it in the holder
+            Log.v(TAG, "inflating view.");
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.row_layout, null);
-            viewHolder.view = convertView;
+
+            viewHolder = new ViewHolder();
+
+            Random rand = new Random(position);
+            TextView textView = (TextView) convertView.findViewById(R.id.stateName);
+            textView.setText(values[position]);
+            Log.v(TAG, "Setting state name to : " + values[position]);
+            viewHolder.stateName = textView;
+
+            View leftBox = convertView.findViewById(R.id.leftBox);
+            leftBox.setBackgroundColor(rand.nextInt());
+            viewHolder.leftBox = leftBox;
+
+            View right = convertView.findViewById(R.id.rightBox);
+            right.setBackgroundColor(rand.nextInt());
+            viewHolder.rightBox = right;
+
+            convertView.setTag(viewHolder);
         } else {
-            convertView = viewHolder.view;
+            viewHolder = (ViewHolder) convertView.getTag();
         }
-
-        Random rand = new Random(position);
-        TextView textView = (TextView) convertView.findViewById(R.id.stateName);
-        textView.setText(values[position]);
-
-        View leftBox = convertView.findViewById(R.id.left);
-        leftBox.setBackgroundColor(rand.nextInt());
-
-        View right = convertView.findViewById(R.id.right);
-        right.setBackgroundColor(rand.nextInt());
-        convertView.setTag(viewHolder);
 
         if (position % 2 == 0) {
             convertView.setBackgroundColor(Color.rgb(231, 249, 255));
         } else {
             convertView.setBackgroundColor(Color.rgb(195, 240, 255));
         }
+
+        viewHolder.stateName.setText(values[position]);
 
         return convertView;
     }
