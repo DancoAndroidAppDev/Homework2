2471016a0c8d8149ff2a5751947948ef434afbd6
costd035 <daniel.x.costinett.-nd@disney.com>
2014-10-24 09:12:04 -0700

    add layout xml files, work on Adapter implementation

--- app/src/main/java/net/cozz/danco/homework2/OptimizedCustomAdapter.java
+++ app/src/main/java/net/cozz/danco/homework2/OptimizedCustomAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -16,11 +17,15 @@ public class OptimizedCustomAdapter extends ArrayAdapter<String> {
 
     private static ViewHolder viewHolder;
     private Context context;
+    private String[] values;
 
-    public OptimizedCustomAdapter(Context context, int resource, List objects) {
-        super(context, resource, objects);
+
+
+    public OptimizedCustomAdapter(Context context, String[] objects) {
+        super(context, R.layout.odd_row_layout, objects);
         viewHolder = new ViewHolder();
         this.context = context;
+        this.values = objects;
     }
 
     static class ViewHolder {
@@ -32,19 +37,22 @@ public class OptimizedCustomAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        View rowView = null;
         if (convertView == null) {
             // inflate the view and put it in the holder
             LayoutInflater inflater = LayoutInflater.from(context);
 
             if (position % 2 == 0) {
-                convertView = inflater.inflate(R.layout.even_row_layout, null);
-                ViewHolder.evenView = (TextView) convertView;
-                convertView.setTag(viewHolder);
+                rowView = inflater.inflate(R.layout.even_row_layout, parent);
+                rowView.setTag(viewHolder);
             } else {
-                convertView = inflater.inflate(R.layout.odd_row_layout, null);
-                ViewHolder.oddView = (TextView) convertView;
+                rowView = inflater.inflate(R.layout.odd_row_layout, null);
                 convertView.setTag(viewHolder);
             }
+
+            TextView textView = (TextView) rowView.findViewById(R.id.stateName);
+            View leftBox = rowView.findViewById(R.id.left);
+            View right = rowView.findViewById(R.id.right);
 
         } else {
             convertView = position %2 == 0 ? ViewHolder.evenView : ViewHolder.oddView;
